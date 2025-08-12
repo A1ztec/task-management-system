3b35d05b083e426270bba9d4c1ee50cc802ee0f9
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-12 21:56:22 +0300

    Refactor ApiStatus enum for consistent formatting and indentation

--- app/Enums/System/ApiStatus.php
+++ app/Enums/System/ApiStatus.php
@@ -4,35 +4,34 @@
 namespace App\Enums\System;
 
 
-  enum ApiStatus : string
+enum ApiStatus: string
 {
-  case SUCCESS = 'success';
-  case ERROR = 'error';
+    case SUCCESS = 'success';
+    case ERROR = 'error';
 
-  case VALIDATION_FAILED = 'validation_failed',
+    case VALIDATION_FAILED = 'validation_failed';
 
-  case UNAUTHORIZED = 'unauthorized';
+    case UNAUTHORIZED = 'unauthorized';
 
-  case NOT_FOUND = 'not_found';
-
-
-
-  public function message() : string
-  {
-      return match ($this){
-
-        self::SUCCESS => __('Request Successful'),
-        self::ERROR => __('Error Accurred'),
-        self::VALIDATION_FAILED => __('Validation Failed'),
-        self::UNAUTHORIZED => __('Unauthorized Access'),
-        self::NOT_FOUND => __('Resource Not Found')
-    };
-  }
+    case NOT_FOUND = 'not_found';
 
 
-  public static function options() : array
-  {
-       return collect(self::cases())->mapWithKeys(fn($item) => [$item->value => $item->message()])->toArray();
-  }
 
+    public function message(): string
+    {
+        return match ($this) {
+
+            self::SUCCESS => __('Request Successful'),
+            self::ERROR => __('Error Accurred'),
+            self::VALIDATION_FAILED => __('Validation Failed'),
+            self::UNAUTHORIZED => __('Unauthorized Access'),
+            self::NOT_FOUND => __('Resource Not Found')
+        };
+    }
+
+
+    public static function options(): array
+    {
+        return collect(self::cases())->mapWithKeys(fn($item) => [$item->value => $item->message()])->toArray();
+    }
 }
