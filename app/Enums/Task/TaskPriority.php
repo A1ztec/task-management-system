0e054b341b0f78176df754c9ccda123d16c9fd88
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-14 00:35:31 +0300

    add options function for task priority

--- app/Enums/Task/TaskPriority.php
+++ app/Enums/Task/TaskPriority.php
@@ -9,4 +9,15 @@ enum TaskPriority: string
     case LOW = 'low';
     case MEDIUM = 'medium';
     case HIGH = 'high';
+
+
+
+    public static function options(): array
+    {
+        return [
+            self::LOW->value => 'Low',
+            self::MEDIUM->value => 'Medium',
+            self::HIGH->value => 'High',
+        ];
+    }
 }
