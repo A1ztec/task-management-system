0e82dbc00bd99adb4ece08f6cb5dec5383afe091
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-13 23:23:27 +0300

    inject the Use Repositiory and the User Service in the service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,10 +5,13 @@ namespace App\Providers;
 use App\Enums\User\UserRole;
 use App\Services\TaskService;
 use App\Repositories\TaskRepository;
+use App\Services\UserService;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\TaskRepositoryInterface;
+use App\Repositories\UserRepository;
+use App\Repositories\UserRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
 
         $this->app->bind(TaskService::class, function ($app) {
             return new TaskService($app->make(TaskRepositoryInterface::class));
+        });
+
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+
+        $this->app->bind(UserService::class, function ($app) {
+            return new UserService($app->make(UserRepositoryInterface::class));
         });
     }
 
