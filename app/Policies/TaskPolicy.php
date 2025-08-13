54a452b4706b1799d7103f1156888d7307c21572
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-13 03:47:43 +0300

    Update viewAny method in TaskPolicy to restrict access to admins only

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -14,7 +14,7 @@ class TaskPolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->isAdmin() || $user->tasks()->exists();
+        return $user->isAdmin();
     }
 
     /**
