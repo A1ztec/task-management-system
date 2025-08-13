cd9e171a0c7b1df051ea81be7ddafdb37b77fc6e
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-13 14:23:26 +0300

    fix syntax  issues and  wrong logic implementation in the AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,6 +7,7 @@ use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Traits\ApiResponseTrait;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -21,32 +22,32 @@ class AuthController extends Controller
         $data = $request->validated();
 
         try {
-            $user = User::wehre('email', $data['email']);
+            $user = User::where('email', $data['email'])->first();
 
             if (!$user) {
                 return $this->notFoundResponse(message: __('user not found'));
             }
 
-            if (!Hash::check($data['password'], $user->passowrd)) {
+            if (!Hash::check($data['password'], $user->password)) {
                 return $this->errorResponse(message: __('Wrong Credintials'));
             }
 
-
-
-            $token = $user->createToken()->plainTextToken;
+            $token_name = $user->name . now() . 'login_token';
+            $user->tokens()->where('name', $token_name)->delete();
+            $token = $user->createToken($token_name)->plainTextToken;
 
             $data = [
                 'user' => $user,
                 'token' => $token
             ];
 
-            Log::info(message : __('user logged in successfully') , [
-                'email' => $user->email;
+            Log::info(message: __('user logged in successfully'), context: [
+                'email' => $user->email,
                 'ip' => $request->ip()
             ]);
-            return $this->successResponse(message: __('User Logged In Successfully'));
+            return $this->successResponse(message: __('User Logged In Successfully'), data: $data);
         } catch (Exception $e) {
-            $this->logAndReturnErrorResponse($e->getMessage());
+            return $this->logAndReturnErrorResponse($e->getMessage());
         }
     }
 
@@ -58,7 +59,7 @@ class AuthController extends Controller
             return $this->errorResponse(message: __('User Not Authenticated'));
         }
 
-        $user->currentAccessToken()->delete;
+        $user->currentAccessToken()->delete();
 
         return $this->successResponse(message: __('User Logged Out Successfully'));
     }
