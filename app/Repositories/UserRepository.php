72e1e4d8d642fde2e72a4e04cef6c8b746358ab9
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-13 23:24:38 +0300

    created user repository for the data access layer

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserRepository implements UserRepositoryInterface
+{
+    public function listAll(): mixed
+    {
+        $users =  User::paginate();
+        return $users;
+    }
+
+    public function show(User $user): User
+    {
+        return $user;
+    }
+
+    public function create($data): User
+    {
+        $user =  User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'email_verified_at' => now(),
+        ]);
+
+        return $user;
+    }
+
+    public function update($data, User $user): User
+    {
+        $user->update($data);
+        $user = $user->fresh();
+        return $user;
+    }
+
+    public function delete(User $user): bool
+    {
+        $delete = $user->delete();
+        return $delete;
+    }
+}
