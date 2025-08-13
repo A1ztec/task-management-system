c0fe834db793072ccd54b8f6dafd023be0bd396d
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-13 23:41:18 +0300

    add UpdateUserRequest for user update validation

---
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\user;
+
+use App\Enums\User\UserRole;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['nullable', 'string', 'max:255'],
+            'email' => ['nullable', 'string', 'email', 'max:255', 'unique:users,email,' . $this->user->id],
+            'password' => ['nullable', 'string', Password::min(8)->mixedCase()->numbers()->symbols(), 'confirmed'],
+            'role' => ['nullable', 'string', Rule::enum(UserRole::class)],
+        ];
+    }
+}
