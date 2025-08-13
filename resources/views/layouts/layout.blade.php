98db78838999861b12745714094cc4f116970ca8
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-14 00:37:44 +0300

    fixed the route of task management in layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -56,9 +56,9 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="{{ url('dashboard') }}" class="brand-link">
+        <a href="{{ route('admin.dashboard') }}" class="brand-link">
             <img src="{{ asset('adminlte/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">Task Mangement System</span>
+            <span class="brand-text font-weight-light">Task Management System</span>
         </a>
 
         <!-- Sidebar -->
@@ -95,7 +95,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        <a href="{{ route('admin.tasks.index') }}" class="nav-link">
                             <i class="nav-icon fas fa-tasks"></i>
                             <p>Task Management</p>
                         </a>
