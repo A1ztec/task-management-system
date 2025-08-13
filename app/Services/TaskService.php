230bdde5c69b1a51943ccbb9d1088b83d990c483
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-13 05:03:46 +0300

    Add TaskService for task management operations

---
+++ app/Services/TaskService.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Services;
+
+use App\Models\Task;
+use App\Repositories\BaseTaskRepository;
+use App\Repositories\TaskRepositoryInterface;
+
+class TaskService
+{
+
+    public function __construct(public TaskRepositoryInterface $repository) {}
+
+
+    public function listAll()
+    {
+
+        return $this->repository->listAll();
+    }
+
+
+    public function create($data)
+    {
+        return $this->repository->create($data);
+    }
+
+
+    public function update($data, Task $task) : Task
+    {
+      return $this->repository->update($data, $task);
+    }
+
+    public function delete(Task $task)
+    {
+        return $this->repository->delete($task);
+    }
+}
