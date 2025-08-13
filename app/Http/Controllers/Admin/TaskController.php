61542428c33794d74588307bcb4e1929747d0b15
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-14 00:36:21 +0300

    implemented the task controller with the logic

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -2,10 +2,79 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use Exception;
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Services\TaskService;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Task\TaskRequest;
 
 class TaskController extends Controller
 {
-    //
+
+    public function __construct(public TaskService $service) {}
+
+    public function listAll()
+    {
+        $tasks = $this->service->listAll();
+        return view('admin.task.index', compact('tasks'));
+    }
+
+    public function show(Task $task)
+    {
+        $task = $this->service->show($task);
+
+        return view('admin.task.show', ['task' => $task]);
+    }
+
+    public function create()
+    {
+        $users = User::all();
+        return view('admin.task.create', ['users' => $users]);
+    }
+
+    public function store(TaskRequest $request)
+    {
+        $data = $request->validated();
+        try {
+            $task = $this->service->create($data);
+            flash()->success('Task Created Successfully');
+            return redirect()->route('admin.tasks.index');
+        } catch (Exception $e) {
+            flash()->error($e->getMessage());
+            return redirect()->route('admin.tasks.create')->withInput();
+        }
+    }
+
+    public function edit(Task $task)
+    {
+        $users = User::all();
+        return view('admin.task.edit', ['task' => $task, 'users' => $users]);
+    }
+
+    public function update(TaskRequest $request, Task $task)
+    {
+        $data = $request->validated();
+        try {
+            $this->service->update($task, $data);
+            flash()->success('Task Updated Successfully');
+            return redirect()->route('admin.tasks.index');
+        } catch (Exception $e) {
+            flash()->error($e->getMessage());
+            return redirect()->route('admin.tasks.edit', ['task' => $task])->withInput();
+        }
+    }
+
+    public function destroy(Task $task)
+    {
+        try {
+            $this->service->delete($task);
+            flash()->success('Task Deleted Successfully');
+            return redirect()->route('admin.tasks.index');
+        } catch (Exception $e) {
+            flash()->error($e->getMessage());
+            return redirect()->route('admin.tasks.index');
+        }
+    }
 }
